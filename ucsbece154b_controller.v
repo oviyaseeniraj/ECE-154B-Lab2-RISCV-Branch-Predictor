95e1c10ec73649c6c25743397f7a2379d56ca734
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-04-18 12:17:09 -0700

    revert

--- ucsbece154b_controller.v
+++ ucsbece154b_controller.v
@@ -104,7 +104,6 @@ module ucsbece154b_controller (
         //     `endif  
        endcase
     default: 
-      ALUControlD = ALUcontrol_add;
       `ifdef SIM
           $warning("Unsupported ALUop given: %h", ALUOpD);
       `else
