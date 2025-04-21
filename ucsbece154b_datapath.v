86964b894ada0d18bf7f6a59c634ffd29442aa64
Justin Wu <justin_wu@ucsb.edu>
2025-04-21 16:31:13 -0700

    revert datapath

--- ucsbece154b_datapath.v
+++ ucsbece154b_datapath.v
@@ -1,13 +1,3 @@
-// ucsbece154b_datapath.v
-// ECE 154B, RISC-V pipelined processor 
-// All Rights Reserved
-// Copyright (c) 2024 UCSB ECE
-// Distribution Prohibited
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// TO DO: MODIFY FETCH AND EXECUTE STAGE BELOW TO IMPLEMENT BRANCH PREDICTOR
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 module ucsbece154b_datapath (
     input                clk, reset,
     input                PCSrcE_i,
@@ -43,7 +33,6 @@ module ucsbece154b_datapath (
 
 `include "ucsbece154b_defines.vh"
 
-// Define signals earleir if needed here
 wire [31:0] PCTargetE;
 reg [31:0] ResultW;
 
@@ -53,35 +42,16 @@ parameter NUM_GHR_BITS = 5;
 wire [31:0] BTBtargetF;
 wire BranchTakenF;
 wire [NUM_GHR_BITS-1:0] PHTreadaddressF;
-wire [NUM_GHR_BITS-1:0] PHTreadaddressD;  // Pipeline register for PHT index
-wire [NUM_GHR_BITS-1:0] PHTwriteaddressE; // Pipeline register for PHT index
-wire [31:0] BTBwritedataE; // Pipeline register for BTB data
-wire [4:0] BTBwriteaddressE; // Pipeline register for BTB address
-wire BTBweE; // Pipeline register for BTB write enable
-wire PHTweE; // Pipeline register for PHT write enable
-wire PHTincrementE; // Pipeline register for PHT increment
-wire GHRresetE; // Pipeline register for GHR reset
-
-
-// ***** FETCH STAGE *********************************
-
-// Mux feeding to PC
 wire [31:0] PCPlus4F = PCF_o + 32'd4;
 
-// New PC selection logic:
-// 1. Misprediction correction (PCSrcE_i): Use execute stage target
-// 2. Predicted taken: Use BTB target
-// 3. Default: PC+4
 wire [31:0] PCnewF = PCSrcE_i ? PCTargetE : 
-                    (BranchTakenF ? BTBtargetF : PCPlus4F);
+                   (BranchTakenF ? BTBtargetF : PCPlus4F);
 
-// Update registers
 always @ (posedge clk) begin
     if (reset)        PCF_o <= pc_start;
     else if (!StallF_i) PCF_o <= PCnewF;
 end
 
-// ***** DECODE STAGE ********************************
 reg [31:0] InstrD, PCPlus4D, PCD;
 wire [4:0] RdD;
 
@@ -93,7 +63,6 @@ assign Rs1D_o = InstrD[19:15];
 assign Rs2D_o = InstrD[24:20];
 assign RdD = InstrD[11:7];
 
-// Register File
 wire [31:0] RD1D, RD2D;
 ucsbece154b_rf rf (
     .clk(~clk),
@@ -102,7 +71,6 @@ ucsbece154b_rf rf (
     .we3_i(RegWriteW_i), .wd3_i(ResultW)
 );
 
-// Sign extension
 reg [31:0] ExtImmD;
 
 always @ * begin
@@ -112,16 +80,10 @@ always @ * begin
       imm_Btype: ExtImmD = {{20{InstrD[31]}},InstrD[7],InstrD[30:25], InstrD[11:8],1'b0};
       imm_Jtype: ExtImmD = {{12{InstrD[31]}},InstrD[19:12],InstrD[20],InstrD[30:21],1'b0};
       imm_Utype: ExtImmD = {InstrD[31:12],12'b0};
-      default:   ExtImmD = 32'bx; 
-//            `ifdef SIM
-//            $warning("Unsupported ImmSrc given: %h", ImmSrc_i);
-//            `else
-//            ;
-//            `endif
+      default:   ExtImmD = 32'bx;
    endcase
 end
 
-// Update registers
 always @ (posedge clk) begin
     if (reset | FlushD_i) begin
         InstrD   <= 32'b0;
@@ -134,16 +96,9 @@ always @ (posedge clk) begin
     end 
 end
 
-// Branch Predictor Update Logic
-assign is_branchD = (op_o == instr_branch_op);
-assign is_jumpD = (op_o == instr_jal_op) || (op_o == instr_jalr_op); // not sure if this will work the "instr" pieces are from controller, also defined branch and jump there
-
-// ***** EXECUTE STAGE ******************************
 reg [31:0] RD1E, RD2E, PCPlus4E, ExtImmE, PCE; 
-reg is_branchE, is_jumpE;
 reg [31:0] ForwardDataM;
 
-// Forwarding muxes 
 reg  [31:0] SrcAE;
 always @ * begin
     case (ForwardAE_i)
@@ -165,8 +120,6 @@ always @ * begin
     endcase
 end
 
-
-// Mux feeding ALU Src B
 always @ * begin
     case (ALUSrcE_i)
         SrcB_imm: SrcBE = ExtImmE;
@@ -175,8 +128,6 @@ always @ * begin
     endcase
 end
 
-
-// ALU
 wire [31:0] ALUResultE;
 ucsbece154b_alu alu (
     .a_i(SrcAE), .b_i(SrcBE),
@@ -185,20 +136,16 @@ ucsbece154b_alu alu (
     .zero_o(ZeroE_o)
 );
 
-// Branch Predictor Update Logic
-assign BTBwritedataE = {PCE[31:$clog2(NUM_BTB_ENTRIES)+2], ALUResultE, is_jumpE, is_branchE};
-assign BTBwriteaddressE = PCE[$clog2(NUM_BTB_ENTRIES)+1:2];
-assign BTBweE = (is_jumpE || (is_branchE && PCSrcE_i));
-assign PHTweE = is_branchE;
-assign PHTincrementE = PCSrcE_i;
-assign PHTwriteaddressE = PHTreadaddressD;
-assign GHRresetE = PCSrcE_i;
+wire is_branchE = (op_o == instr_branch_op);
+wire is_jumpE = (op_o == instr_jal_op) || (op_o == instr_jalr_op);
+wire [4:0] BTBwriteaddressE = PCD[$clog2(NUM_BTB_ENTRIES)+1:2];
+wire BTBweE = (is_jumpE || (is_branchE && PCSrcE_i));
+wire PHTweE = is_branchE;
+wire PHTincrementE = PCSrcE_i;
+wire GHRresetE = PCSrcE_i;
 
-// PC Target
 assign PCTargetE = PCE + ExtImmE;
 
-
-// Update registers
 always @ (posedge clk) begin
     if (reset | FlushE_i) begin
         RD1E     <= 32'b0;
@@ -209,8 +156,6 @@ always @ (posedge clk) begin
         Rs1E_o   <=  5'b0;
         Rs2E_o   <=  5'b0;
         RdE_o    <=  5'b0;
-        is_branchE <= 1'b0;
-        is_jumpE   <= 1'b0;
     end else begin 
         RD1E     <= RD1D;
         RD2E     <= RD2D;
@@ -220,8 +165,6 @@ always @ (posedge clk) begin
         Rs1E_o   <= Rs1D_o;
         Rs2E_o   <= Rs2D_o;
         RdE_o    <= RdD;
-        is_branchE <= is_branchD;
-        is_jumpE   <= is_jumpD;
     end 
 end
 
@@ -233,7 +176,7 @@ ucsbece154b_branch #(
     .reset_i(reset),
     .pc_i(PCF_o),
     .BTBwriteaddress_i(BTBwriteaddressE),
-    .BTBwritedata_i(BTBwritedataE),
+    .BTBwritedata_i(ALUResultE),
     .BTBtarget_o(BTBtargetF),
     .BTB_we(BTBweE),
     .BranchTaken_o(BranchTakenF),
@@ -241,11 +184,10 @@ ucsbece154b_branch #(
     .PHTincrement_i(PHTincrementE),
     .GHRreset_i(GHRresetE),
     .PHTwe_i(PHTweE),
-    .PHTwriteaddress_i(PHTwriteaddressE),
+    .PHTwriteaddress_i(PHTreadaddressF),
     .PHTreadaddress_o(PHTreadaddressF)
 );
 
-// ***** MEMORY STAGE ***************************
 reg [31:0] ExtImmM, PCPlus4M;
 
 always @ * begin
@@ -254,11 +196,9 @@ always @ * begin
      MuxResult_PCPlus4: ForwardDataM = PCPlus4M;
      MuxResult_imm:     ForwardDataM = ExtImmM;
      default:           ForwardDataM = 32'bx;
-
    endcase
  end
 
-// Update registers
 always @ (posedge clk) begin
     if (reset) begin
         ALUResultM_o <= 32'b0;
@@ -275,7 +215,6 @@ always @ (posedge clk) begin
     end 
 end
 
-// ***** WRITEBACK STAGE ************************
 reg [31:0] PCPlus4W, ALUResultW, ReadDataW, ExtImmW;
 
 always @ * begin
@@ -285,17 +224,9 @@ always @ * begin
      MuxResult_PCPlus4:  ResultW = PCPlus4W;
      MuxResult_imm:  ResultW = ExtImmW;
      default:        ResultW = 32'bx;
-  //          `ifdef SIM
-  //          $warning("Unsupported ResultSrc given: %h", ResultSrc_i);
-  //          `else
-  //          ;
-  //          `endif
-
-  //   end
    endcase
  end
 
-// Update registers
 always @ (posedge clk) begin
     if (reset) begin
         ALUResultW <= 32'b0;
@@ -311,6 +242,5 @@ always @ (posedge clk) begin
         RdW_o      <= RdM_o;
     end 
 end
-
 
 endmodule
