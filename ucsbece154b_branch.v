d6314dca1d3897c3fd54882b8732c9cceea6b6bc
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-04-22 16:34:03 -0700

    removed extra match logic and printing btb valid

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -58,12 +58,11 @@ always @(posedge clk) begin
     $display("[BTB INDEX] index=%0d", btb_index);
     $display("[BTB TAG FROM PC] tag=%h", btb_tag_in);
     $display("[BTB TAG FROM TABLE] tag=%h", BTB_tag[btb_index]);
+    $display("[BTB VALID] valid=%b", BTB_valid[btb_index]);
     tag_match <= BTB_valid[btb_index];
-    if (BTB_valid[btb_index]) begin
+    if (BTB_valid[btb_index])
         tag_match <= (btb_tag_in == BTB_tag[btb_index]);
-    end else begin
-        tag_match <= 1'b0;
-    end
+
     $display("[BTB TAG MATCH] match=%b", tag_match);
     btb_entry_valid <= BTB_valid[btb_index];
 
