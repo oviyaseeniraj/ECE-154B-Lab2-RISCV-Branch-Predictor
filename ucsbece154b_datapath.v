da1b73adc45ab601285a7af9f371dd2312c39459
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-04-22 01:28:00 -0700

    missed a bit reversion

--- ucsbece154b_datapath.v
+++ ucsbece154b_datapath.v
@@ -51,7 +51,7 @@ reg [4:0] BTBwriteaddrE;
 reg [31:0] BTBwritedataE;
 
 // NEW: Branch predictor instantiation
-ucsbece154b_branch #(64, 6) branch_predictor (
+ucsbece154b_branch #(32, 5) branch_predictor (
     .clk(clk),
     .reset_i(reset),
     .pc_i(PCF_o),
