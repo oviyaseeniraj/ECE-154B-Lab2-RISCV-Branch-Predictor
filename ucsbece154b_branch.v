126d1f92b823c62584265f9fb90d449f9f131b89
Justin Wu <justin_wu@ucsb.edu>
2025-04-22 19:29:33 -0700

    add btb_valid wire to waveform

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -144,6 +144,7 @@ wire [31:0] btb_target_bypass = (BTB_we && BTBwriteaddress_i == btb_index) ?
 
 assign btb_b = BTB_b_flag[btb_index];
 assign btb_j = BTB_j_flag[btb_index];
+assign btb_valid = BTB_valid[btb_index];
 
 always @(*) begin
     if (tag_match && BTB_valid[btb_index]) begin
