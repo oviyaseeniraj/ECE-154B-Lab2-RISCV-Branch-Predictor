581a8848bd6568c15ea9294bbd405f3369112850
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-04-22 19:32:36 -0700

    switched where prints occur

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -101,15 +101,16 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    $display("[BTB INDEX] index=%0d", btb_index);
-    $display("[BTB TAG FROM PC] tag=%h", btb_tag_in);
-    $display("[BTB TAG FROM TABLE] tag=%h", BTB_tag[btb_index]);
-    $display("[BTB VALID] valid=%b", BTB_valid[btb_index]);
     if (BTB_valid[btb_index]) begin
         tag_match <= (btb_tag_in == BTB_tag[btb_index]);
     end else begin
         tag_match <= 1'b0;
     end
+    
+    $display("[BTB INDEX] index=%0d", btb_index);
+    $display("[BTB TAG FROM PC] tag=%h", btb_tag_in);
+    $display("[BTB TAG FROM TABLE] tag=%h", BTB_tag[btb_index]);
+    $display("[BTB VALID] valid=%b", BTB_valid[btb_index]);
 
     $display("[BTB TAG MATCH] match=%b", tag_match);
     $display("[BTB TAG E MATCH] match_e=%b", tag_match_e);
