4cafca69d0a4e9060a38459f8c47222fdbd7a350
Justin Wu <justin_wu@ucsb.edu>
2025-04-22 20:21:15 -0700

    ;

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -95,7 +95,7 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    tag_match_d <= tag_match
+    tag_match_d <= tag_match;
     tag_match_e <= tag_match_d;
     op_e <= op_i;
 end
