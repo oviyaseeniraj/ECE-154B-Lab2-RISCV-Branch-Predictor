77ba11feedd6e73350898c05f1db95b0062bdd24
Justin Wu <justin_wu@ucsb.edu>
2025-04-22 20:18:23 -0700

    try updating tagmatch w/ always*

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -95,8 +95,17 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    tag_match_e <= tag_match;
+    tag_match_d <= tag_match
+    tag_match_e <= tag_match_d;
     op_e <= op_i;
+end
+
+always @(*) begin
+    if (BTB_valid[btb_index]) begin
+        tag_match = (btb_tag_in == BTB_tag[btb_index]);
+    end else begin
+        tag_match = 1'b0;
+    end
 end
 
 always @(posedge clk) begin
@@ -104,12 +113,6 @@ always @(posedge clk) begin
     // $display("[BTB TAG FROM PC] tag=%h", btb_tag_in);
     // $display("[BTB TAG FROM TABLE] tag=%h", BTB_tag[btb_index]);
     // $display("[BTB VALID] valid=%b", BTB_valid[btb_index]);
-    
-    if (BTB_valid[btb_index]) begin
-        tag_match <= (btb_tag_in == BTB_tag[btb_index]);
-    end else begin
-        tag_match <= 1'b0;
-    end
     
     $display("[BTB TAG E MATCH] match_e=%b", tag_match_e);
     $display("[BTB TAG DECODE MATCH] match=%b", tag_match);
