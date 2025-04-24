747f8d549f3bb535e53ccae605a2a617a7d76bdf
Justin Wu <justin_wu@ucsb.edu>
2025-04-23 17:45:08 -0700

    use misprediction signal instead of pcsrc for pcnewf

--- ucsbece154b_datapath.v
+++ ucsbece154b_datapath.v
@@ -73,7 +73,7 @@ ucsbece154b_branch #(32, 5) branch_predictor (
 // ***** FETCH STAGE *********************************
 wire [31:0] PCPlus4F = PCF_o + 32'd4;
 wire [31:0] PCtargetF = BranchTakenF ? BTBtargetF : PCPlus4F;
-wire [31:0] PCnewF = PCSrcE_i ? PCTargetE : PCtargetF;
+wire [31:0] PCnewF = Mispredict_o ? PCTargetE : PCtargetF;
 
 always @ (posedge clk) begin
     if (reset)        PCF_o <= pc_start;
