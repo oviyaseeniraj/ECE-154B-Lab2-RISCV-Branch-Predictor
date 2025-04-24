aba6887666b54104a7e6b3723662498a740b62b9
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-04-24 11:47:28 -0700

    32, 1

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -1,7 +1,7 @@
 // DEBUG-ENHANCED ucsbece154b_branch.v
 module ucsbece154b_branch #(
-    parameter NUM_BTB_ENTRIES = 2,
-    parameter NUM_GHR_BITS    = 5
+    parameter NUM_BTB_ENTRIES = 32,
+    parameter NUM_GHR_BITS    = 1
 ) (
     input               clk, 
     input               reset_i,
