b26d90804f1a2e58115185a547e91c8cc4d93ddd
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-04-23 18:21:51 -0700

    omg

--- ucsbece154b_branch.v
+++ ucsbece154b_branch.v
@@ -105,7 +105,7 @@ always @(posedge clk) begin
 
     if (BTB_we && (!tag_match_e || !BTB_valid[BTBwriteaddress_i])) begin
         BTB_target[BTBwriteaddress_i] <= BTBwritedata_i;
-        BTB_tag[BTBwriteaddress_i]    <= btb_write_pc;
+        BTB_tag[BTBwriteaddress_i]    <= tag_e;
         BTB_j_flag[BTBwriteaddress_i] <= (op_e == instr_jal_op || op_e == instr_jalr_op);
         BTB_b_flag[BTBwriteaddress_i] <= (op_e == instr_branch_op);
         BTB_valid[BTBwriteaddress_i]  <= 1'b1;
